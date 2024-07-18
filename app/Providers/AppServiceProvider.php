f1fd6c372d3617acb73d11ee0f27c85c25b710da
osmar <osmarrod18@gmail.com>
2024-07-18 19:11:39 +0100

    Refactor service registration to use singleton pattern

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Services\Checkout\CheckoutService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -12,6 +13,10 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->singleton(CheckoutService::class, function ($app) {
+            $pricingRules = config('pricing_rules.rules');
+            return new CheckoutService($pricingRules);
+        });
     }
 
     /**
