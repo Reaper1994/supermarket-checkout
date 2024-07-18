99535b8f81ff7cdebb52136690fc7643b49e306c
osmar <osmarrod18@gmail.com>
2024-07-18 19:10:20 +0100

    implemented checkout controller

---
+++ app/Http/Controllers/CheckoutController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Services\Checkout\CheckoutService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+/**
+ * Class CheckoutController
+ * @package App\Http\Controllers
+ */
+class CheckoutController extends Controller
+{
+    protected CheckoutService $checkoutService;
+
+    public function __construct(CheckoutService $checkoutService)
+    {
+        $this->checkoutService = $checkoutService;
+    }
+
+    /**
+     * Handle the checkout process.
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function checkout(Request $request): JsonResponse
+    {
+        $productsData = $request->input('products');
+        $pricingRules = config('pricing_rules.rules');
+
+        $co = new CheckoutService($pricingRules);
+
+        foreach ($productsData as $productData) {
+            $product = Product::where('code', $productData['code'])->first();
+            if ($product) {
+                $co->scan($product);
+            }
+        }
+
+        return response()->json(['total' => $co->total()]);
+    }
+}
